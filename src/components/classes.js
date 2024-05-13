ff161e82ee9a50ee6d6e8c950d8ab6e714a1b153
dom <domi.fischer@hotmail.de>
2024-05-13 10:20:58 +0200

    nodes with placeholders, draggable within container

--- src/components/classes.js
+++ src/components/classes.js
@@ -1,14 +1,25 @@
 // RenderClasses.js
 import React from 'react';
+import '../Classes.css'
+import Draggable from 'react-draggable';
+
 
 const RenderClasses = ({ classes }) => {
   return (
     <>
-      {classes.map((className, index) => (
-        <p key={index}>{className}</p>
+      {classes.map((className) => (
+        <Draggable>
+          <div class="node">
+            <h5 class="card-title">{className}</h5>
+            <p class="descript">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="uri">URI</a>
+          </div>
+          </Draggable>
       ))}
     </>
   );
 };
 
 export default RenderClasses;
+
+
